7af9f7ff0b7437855d1fe8a20e76870e5928dd37
Salem <salem@libyaguide.ly>
2017-04-03 14:34:57 +0200

    Element attrbuties added

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
 function init(){
-var rootLayout = new JSLayout();
-rootLayout.setClassName("rootlayout");
+var rootLayout = new JSLayout({"class": "rootlayout"});
 
-var inusername = new JSLayout();
-inusername.setClassName("inusername");
+var inusername = new JSLayout({"class": "inusername"});
 
 var tv = new JSTextView();
 tv.setText("Username: ");
 
-var username = new JSEditText();
-username.setHint("Enter your username");
+var username = new JSEditText({"placeholder": "Enter your username"});
 
 inusername.add(tv);
 inusername.add(username);
 
-var inpass = new JSLayout();
-inpass.setClassName("inpass");
+var inpass = new JSLayout({"class": "inpass"});
 
 var ps = new JSTextView();
 ps.setText("Password: ");
 
-var password = new JSEditText();
-password.setHint("Enter your Password");
+var password = new JSEditText({"placeholder": "Enter your Password"});
 
 inpass.add(ps);
 inpass.add(password);
@@ -30,18 +25,13 @@ rootLayout.add(inusername);
 rootLayout.add(inpass);
 
 
-var button = new JSButton();
-button.setTitle("Login");
-button.setWidth("155px");
-button.setHeigh("22px");
-button.setClassName("loginbutton");
+var button = new JSButton({"class": "loginbutton","value": "Login" , "style": "width: 155px;height:22px" , "type": "submit"});
 
 button.addOnClickListener(function(){
 	
  var msg = new JSPopUpMessages();
  msg.setMessage("Welcome !");
- var btn = new JSButton();
- btn.setTitle("Close");
+ var btn = new JSButton({"value": "Close","type": "submit"});
  btn.addOnClickListener(function(){
  msg.hide();
  });
